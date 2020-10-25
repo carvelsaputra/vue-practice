2d219e0b2bd5fbcfcce5cdc15e16a9b115bb4cf4
Carvel Saputra <carvel.saputra@gmail.com>
2020-10-25 08:56:48 +0700

    registering component global

---
+++ practice-component/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+ <app-server-status></app-server-status>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      status:'Critical'
+    }
+  },
+}
+</script>
+
