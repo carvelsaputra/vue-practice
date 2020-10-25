f14134a1acb0713b8b33033ae223c0f98af7d843
Carvel Saputra <carvel.saputra@gmail.com>
2020-10-25 09:04:03 +0700

    registering local component

--- practice-component/src/Home.vue
+++ practice-component/src/Home.vue
@@ -1,24 +1,14 @@
 <template>
   <div>
-    <p>Server Status: {{ status }}</p>
-    <hr>
-    <button @click="changeStatus">Change Status</button>
+    <app-server-status v-for="server in 5"></app-server-status>
   </div>
 </template>
 
 <script>
+import ServerStatus from "./ServerStatus.vue";
 export default {
-  name: 'app',
-  data () {
-    return {
-      status:'Critical'
-    }
-  },
-  methods:{
-      changeStatus(){
-          this.status= 'Normal'
-      }
+  components: {
+    "app-server-status": ServerStatus
   }
-}
+};
 </script>
-
