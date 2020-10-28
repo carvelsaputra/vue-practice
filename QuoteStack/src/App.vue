312c1b38f33bba73eff86e699b7029f35b336c67
Carvel Saputra <carvel.saputra@gmail.com>
2020-10-28 23:48:04 +0700

    passing data with props and slot

--- QuoteStack/src/App.vue
+++ QuoteStack/src/App.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="container">
-    
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+import QuoteGrid from "./components/QuoteGrid.vue"
     export default {
-        
+        components:{
+            appQuoteGrid: QuoteGrid
+        },
+        data(){
+            return{
+                maxQuotes: 10,
+                quotes:[
+                    'Just a Quote to see something'
+                ],
+
+            }
+        }
     }
 </script>
 
