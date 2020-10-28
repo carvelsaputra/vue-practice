0e88048dfbdf624d6d366a0a49b84e069a59c9b9
Carvel Saputra <carvel.saputra@gmail.com>
2020-10-29 00:18:57 +0700

    wip : finishing touch and stage management

--- QuoteStack/src/App.vue
+++ QuoteStack/src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <app-quote-grid
+      :quotes="quotes"
+      @quoteDeleted="deleteQuote"
+    ></app-quote-grid>
     <div class="row">
-        <div class="col-sm-12 text-center">
-            <div class="alert alert-info">
-                Info : Klik pada quote untuk menghapus ! 
-            </div>
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">
+          Info : Klik pada quote untuk menghapus !
         </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 import QuoteGrid from "./components/QuoteGrid.vue";
 import NewQuote from "./components/NewQuote.vue";
 export default {
   components: {
+    appHeader: Header,
     appQuoteGrid: QuoteGrid,
     appNewQuote: NewQuote,
   },
@@ -30,9 +36,9 @@ export default {
     newQuote(quote) {
       this.quotes.push(quote);
     },
-    deleteQuote(index){
-        this.quotes.splice(index,1);
-    }
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    },
   },
 };
 </script>
