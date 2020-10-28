5064f2898045d9e04f55b8ef92c7b851ac2d8411
Carvel Saputra <carvel.saputra@gmail.com>
2020-10-29 00:00:07 +0700

    adding quotes with custom event

--- QuoteStack/src/App.vue
+++ QuoteStack/src/App.vue
@@ -1,22 +1,31 @@
 <template>
     <div class="container">
+<app-new-quote @quoteAdded="newQuote"></app-new-quote>
     <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+
 import QuoteGrid from "./components/QuoteGrid.vue"
+import NewQuote from "./components/NewQuote.vue"
     export default {
         components:{
-            appQuoteGrid: QuoteGrid
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
         },
-        data(){
+        data:function(){
             return{
                 maxQuotes: 10,
                 quotes:[
                     'Just a Quote to see something'
                 ],
 
+            }
+        },
+        methods:{
+            newQuote(quote){
+                this.quotes.push(quote);
             }
         }
     }
