cc0be2a803c69e11dae8b83add71f506ea74d1ce
Carvel Saputra <carvel.saputra@gmail.com>
2020-11-13 23:31:36 +0700

    trying global mixins

--- Filtering/src/main.js
+++ Filtering/src/main.js
@@ -4,6 +4,11 @@ import App from './App.vue'
 Vue.filter('to-lowercase',(value)=>{
   return value.toLowerCase();
 })
+Vue.mixin({
+  created(){
+    console.log('global mixin')
+  }
+})
 new Vue({
   el: '#app',
   render: h => h(App)
