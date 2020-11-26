720fbcc9a96fc433d32e424cb102bee1629ab77a
Carvel Saputra <carvel.saputra@gmail.com>
2020-11-26 18:53:44 +0700

    introduce about transition tag

--- Animation/src/App.vue
+++ Animation/src/App.vue
@@ -3,17 +3,24 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
-
+                <hr>
+                <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+                <br><br>
+                <transition>
+                    <div class="alert alert-info" v-if="show">This is Some Info</div>
+                    <div class="alert alert-info" v-if="show">This is Some Info</div>
+                </transition>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+// only can animate 1 element with transision 
     export default {
         data() {
             return {
-
+                show:false
             }
         }
     }
