d8752d98169f59bf3188fb811557b04f20d5e807
Carvel Saputra <carvel.saputra@gmail.com>
2020-11-28 19:34:18 +0700

    transition using dynamic attribute

--- Animation/src/App.vue
+++ Animation/src/App.vue
@@ -1,75 +1,107 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Animations</h1>
-                <hr>
-                <button class="btn btn-primary" @click="show = !show">Show Alert</button>
-                <br><br>
-                <transition name="fade">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr />
+
+        <select v-model="alertAnimation" class="form-control">
+          <option value="fade">fade</option>
+          <option value="slide">slide</option>
+        </select>
+        <br />
+        <button class="btn btn-primary" @click="show = !show">
+          Show Alert
+        </button>
+        <br /><br />
+        <transition :name="alertAnimation">
+          <div class="alert alert-info" v-if="show">This is Some Info</div>
+        </transition>
+        <transition :name="alertAnimation" type="animation">
+          <div class="alert alert-info" v-if="show">This is Some Info</div>
+        </transition>
+        <transition name="slide" type="animation" appear>
+          <div class="alert alert-info" v-if="show">This is Some Info</div>
+        </transition>
+        <!-- <transition 
+                    enter-class=""
+                    enter-active-class="animated bounce"
+                    leave-class=""
+                    leave-active-class="animated shake"
+                    appear
+                        
+                >
                     <div class="alert alert-info" v-if="show">This is Some Info</div>
                 </transition>
-                  <transition name="slide">
-                    <div class="alert alert-info" v-if="show">This is Some Info</div>
-                </transition>
-            </div>
-        </div>
+        <transition
+          appear
+          enter-active-class="animate__animated animate__bounce"
+          leave-active-class="animate__animated animate__shake"
+        > -->
+        <!-- <div class="alert alert-info" v-if="show">This is Some Info</div>
+        </transition> -->
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-// only can animate 1 element with transision 
-    export default {
-        data() {
-            return {
-                show:false
-            }
-        }
-    }
+// only can animate 1 element with transision
+export default {
+  data() {
+    return {
+      show: false,
+      alertAnimation: "fade",
+    };
+  },
+};
 </script>
 
 <style>
 /* fade transition using css property */
-    .fade-enter{
-        opacity: 0;
-    }
-    .fade-enter-active{
-        transition: opacity 1s;
-    }
-    .fade-leave{
-        /* opacity: 1 */
-    }
-    .fade-leave-active{
-        transition: opacity 1s;
-        opacity: 0;
-    }
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 1s;
+}
+.fade-leave {
+  /* opacity: 1 */
+}
+.fade-leave-active {
+  transition: opacity 1s;
+  opacity: 0;
+}
 /* slide transition using css property */
-    .slide-enter{
-        /* transform: translateY(20); */
-    }
-    .slide-enter-active{
-        animation: slide-in 1s ease-out forwards;
-    }
-    .slide-leave{
-
-    }
-    .slide-leave-active{
-        animation: slide-out 1s ease-out forwards;
-    }
-    @keyframes slide-in{
-        from{
-            transform: translateY(20px);
-        }
-        to{
-            transform: translateY(0);
-        }
-    }
-    @keyframes slide-out{
-        from{
-            transform: translateY(0);
-        }
-        to{
-            transform: translateY(20px);
-        }
-    }
+.slide-enter {
+  /* transform: translateY(20); */
+  opacity: 0;
+}
+.slide-enter-active {
+  transition: opacity 0.5s;
+  animation: slide-in 1s ease-out forwards;
+}
+.slide-leave {
+}
+.slide-leave-active {
+  transition: opacity 1s;
+  animation: slide-out 1s ease-out forwards;
+  opacity: 0;
+}
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
+}
 </style>
