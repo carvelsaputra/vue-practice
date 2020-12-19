d4579a21097a3d31feae26b6cb07a966fa58f302
Carvel Saputra <carvel.saputra@gmail.com>
2020-12-19 18:23:00 +0700

    make router link more dynamics

--- route/src/components/user/UserDetail.vue
+++ route/src/components/user/UserDetail.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <h3>Some User Details</h3>
-    <p>User Loaded has ID : {{$route.params.id}}</p>
+    <p>User Loaded has ID : {{ $route.params.id }}</p>
+    <router-link tag="button" :to="'/user/' + $route.params.id + '/edit'" class="btn btn-primary"
+      >Edit User</router-link
+    >
   </div>
-
 </template>
