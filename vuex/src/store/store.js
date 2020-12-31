98e5aa14034361bf37d82dc7251b9ce6e2375d54
Carvel Saputra <carvel.saputra@gmail.com>
2020-12-31 13:33:52 +0700

    reconstruct files and folder vuex

--- vuex/src/store/store.js
+++ vuex/src/store/store.js
@@ -1,33 +1,34 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-import counter from "./modules/counter.js"
+import counter from "./modules/counter.js";
+
+import * as actions from "./actions.js";
+import * as getters from "./getters.js";
+import * as mutations from "./mutations.js";
+
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
     value: 0,
   },
-  getters: {
-    value: (state) => {
-      return state.value;
-    },
+  actions,
+  getters,
+  mutations,
+  modules: {
+    counter,
   },
-  mutations: {
-    updatedValue: (state, payload) => {
-      state.value = payload;
-    },
-  },
-  actions: {
-    //   increment: context => {
-    //       context.commit('increment');
-    //   }
+  // getters: {
 
-    updateValue: ({ commit }, payload) => {
-      commit("updatedValue", payload);
-    },
-  },
-  modules:{
-    counter
-  }
+  // },
+  // mutations: {
+
+  // },
+  // actions: {
+  //   //   increment: context => {
+  //   //       context.commit('increment');
+  //   //   }
+
+  // },
 });
