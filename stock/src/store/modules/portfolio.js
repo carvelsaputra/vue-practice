8578dca56a191f725c62140fc7268c8a19c54bbf
Carvel Saputra <carvel.saputra@gmail.com>
2021-01-15 14:06:13 +0700

    fixed error vuex portfolio

--- stock/src/store/modules/portfolio.js
+++ stock/src/store/modules/portfolio.js
@@ -1,41 +1,44 @@
 const state = {
     funds: 10000,
-    stocks:[]
-}
+    stocks: []
+};
 
 const mutations = {
-    'BUY_STOCK'(state, { stockID, quantity, stockPrice }) {
-        const record = state.stocks.find(element => element.id == stockID)
-        if(record){
-            record.quantity += quantity
-        }
-        else{
+    'BUY_STOCK'(state, {stockId, quantity, stockPrice}) {
+        const record = state.stocks.find(element => element.id == stockId);
+        if (record) {
+            record.quantity += quantity;
+        } else {
             state.stocks.push({
-                id: stockID,
+                id: stockId,
                 quantity: quantity
-            })
+            });
         }
+        state.funds -= stockPrice * quantity;
     },
-    'SELL_STOCK'(state, {stockID, quantity, stockPrice}) {
-        const record = state.stocks.find(element => element.id == stockID)
-        if(record > quantity){
-            record.quantity -= quantity
-        }
-        else{
+    'SELL_STOCK' (state, {stockId, quantity, stockPrice}) {
+        const record = state.stocks.find(element => element.id == stockId);
+        if (record.quantity > quantity) {
+            record.quantity -= quantity;
+        } else {
             state.stocks.splice(state.stocks.indexOf(record), 1);
         }
-        state.funds += stockPrice * quantity
+        state.funds += stockPrice * quantity;
+    },
+    'SET_PORTFOLIO' (state, portfolio) {
+        state.funds = portfolio.funds;
+        state.stocks = portfolio.stockPortfolio ? portfolio.stockPortfolio : [];
     }
-}
+};
 
 const actions = {
-    sellStock({commit}, order){
-        commit('SELL_STOCK',order);
+    sellStock({commit}, order) {
+        commit('SELL_STOCK', order);
     }
-}
+};
 
 const getters = {
-    stockPortfolio (state, getters){
+    stockPortfolio (state, getters) {
         return state.stocks.map(stock => {
             const record = getters.stocks.find(element => element.id == stock.id);
             return {
@@ -44,15 +47,16 @@ const getters = {
                 name: record.name,
                 price: record.price
             }
-        })
+        });
     },
-    funds(state){
-        return state.funds
+    funds (state) {
+      return state.funds;
     }
-}
-export default{
+};
+
+export default {
     state,
-    getters,
+    mutations,
     actions,
-    mutations
+    getters
 }
